736e94d199755e4eeb16ab2b21b7af9eaf8c54e6
Arivazhagan <agan.e.apm@gmail.com>
2025-05-18 17:32:32 +0530

    Add .env pie tokeno1

---
+++ resources/views/pipedrive-panel-test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test Panel</title>
+</head>
+<body>
+    <h2>Static Test Data</h2>
+    <p>Name: {{ $data['name'] }}</p>
+    <p>Email: {{ $data['email'] }}</p>
+    <p>Message: {{ $data['message'] }}</p>
+</body>
+</html>
